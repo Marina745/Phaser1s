796c2234bad861a8b3b2c57ad6ef22bdc5d82712
Marina745 <158029971+Marina745@users.noreply.github.com>
2024-02-07 10:43:26 +0200

    3
    Оновили сцену, додали платформи та тепер в нас працює фізика

--- game.js
+++ game.js
@@ -3,15 +3,21 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
         update: update
     }
 };
-
 var game = new Phaser.Game(config);
-
+//Завантажили файли для графікі
 function preload ()
 {
     this.load.image('sky', 'assets/sky.png');
@@ -23,12 +29,17 @@ function preload ()
         { frameWidth: 32, frameHeight: 48 }
     );
 }
+var platforms;
+
 function create ()
 {
     this.add.image(400, 300, 'sky');
-    this.add.image(400, 300, 'star');
+    platforms = this.physics.add.staticGroup();
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 }
-
 function update ()
 {
 }
